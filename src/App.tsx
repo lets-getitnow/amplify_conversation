de2d92688f567f0aabb0fc2361f8f265686a5377
lets-getitnow <hunter.peress@icloud.com>
2025-04-12 17:30:08 -0400

    remove todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,11 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      <h1>Welcome to Amplify App</h1>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
